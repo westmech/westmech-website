bf7bd0290a85ebe9630380eb829f5bbcb7d88cc4
Jessica Chen <jesschen046@gmail.com>
2025-07-07 23:20:14 -0400

    routing change removed members portal intro page

--- westmech-website/src/app/members-portal/page.jsx
+++ westmech-website/src/app/members-portal/page.jsx
@@ -1,62 +1,28 @@
-"use client";
 import React from "react";
-import TitleView from "@/components/members-portal/TitleView";
-import ProfileButton from "@/components/members-portal/ProfileButton";
-import { useRouter } from "next/navigation";
+import SignIn from "@/components/members-portal/SignIn";
 
 const page = () => {
-  const router = useRouter();
   const bg = "#F4F4F6";
-  const darkblue = "#017FE0";
-  const purple = "#7F48D7";
-  function handleNavigate(userType) {
-    if (userType == "parent") {
-      router.push("members-portal/parents-portal");
-    } else {
-      router.push("members-portal/students-portal");
-    }
-  }
+
   return (
     <main
       style={{ backgroundColor: bg }}
-      className="sm:min-h-screen w-screen flex flex-col sm:flex-row"
+      className="sm:min-h-screen w-screen flex flex-col sm:flex-row overflow-y-auto"
     >
-      <TitleView
-        title="MEMBERS PORTAL"
-        logo="/login/westmech logo.svg"
-      ></TitleView>
+      <div className="flex flex-col sm:w-[50%] bg-gradient-to-b from-[#017FE0] to-[#5EACE9] items-center justify-center sm:mt-[-100px] mt-[100px] sm:py-0 py-10">
+          <img
+            className="w-[107px] h-[83px] sm:w-[215px] sm:h-[166px]"
+            src={"/login/westmech logo.svg"}
+            alt="Sign in"
+          />
+          <p className="text-white text-[35px] sm:text-[70px] font-bold sm:inline hidden">SIGN IN</p>
+
+      </div>
+      {/*<TitleView user="parent" title="SIGN IN" logo="/login/westmech logo.svg"></TitleView>*/}
       <div
-        style={{ backgroundColor: bg }}
-        className="mt-8 mb-16 sm:mt-4 sm:mb-8 sm:h-screen flex items-center justify-center sm:w-1/2"
+        className="mb-16 sm:h-screen h-1/2 sm:w-[50%] flex sm:mt-0 mt-10 justify-center items-center"
       >
-        <div className="flex flex-col w-[80%]">
-          <h1 className="text-[28px] sm:text-[42px] font-medium">Welcome</h1>
-          <p className="text-[18px]  sm:text-[24px] mb-5">
-            Select a sign in option to get started
-          </p>
-          <ProfileButton
-            onClick={() => handleNavigate("parent")}
-            className="mb-5"
-            user="Parent"
-            iconSrc="/login/person.svg"
-            colour={darkblue}
-          ></ProfileButton>
-          <ProfileButton
-            onClick={() => handleNavigate("student")}
-            user="Student"
-            iconSrc="/login/student.svg"
-            colour={purple}
-          ></ProfileButton>
-          <p className="text-[18px] sm:text-[24px] mb-1">
-            Don't have an account?
-          </p>
-          <a
-            href="#"
-            className="text-[18px] sm:text-[24px] font-medium text-[#017FE0] hover:text-[black] cursor-pointer inline-block"
-          >
-            Ask your coach
-          </a>
-        </div>
+        <SignIn colour="#017FE0" user="parent"></SignIn>
       </div>
     </main>
   );
--- westmech-website/src/components/members-portal/SignIn.jsx
+++ westmech-website/src/components/members-portal/SignIn.jsx
@@ -14,18 +14,18 @@ const SignIn = ({ colour, user }) => {
   })
   const greyBorder = "rgb(209 213 219 / var(--tw-border-opacity))";
   return (
-    <div className="flex flex-col w-[80%] gap-[12px]">
+    <div className="flex flex-col w-[70%] gap-[12px]">
       <form className="flex flex-col gap-[12px]">
-        <h1 className="text-[28px] sm:text-[36px]">Sign In</h1>
+        <h1 className="text-[28px] sm:text-[36px] sm:text-left text-center">Sign In</h1>
         <input
           placeholder="Email"
           type="email"
           required
           onFocus={(e) => (e.target.style.borderColor = colour)}
           onBlur={(e) => (e.target.style.borderColor = greyBorder)}
-          className={`border-[1.5px] sm:text-[18px] border-gray-300 focus:outline-none rounded-md p-3 w-[80%]`}
+          className={`border-[1.5px] sm:text-[18px] border-gray-300 focus:outline-none rounded-md p-3`}
         ></input>
-        <div className="relative w-[80%]">
+        <div className="relative">
           <input
             placeholder="Password"
             type={`${visible ? "text" : "password"}`}
@@ -49,10 +49,10 @@ const SignIn = ({ colour, user }) => {
         <button style={{background:colour}} 
          onMouseEnter={(e) => (e.target.style.backgroundColor = altColour)}
          onMouseLeave={(e) => (e.target.style.backgroundColor = colour)}
-        className={`text-[18px] sm:text-[18px] text-white rounded-md p-3 w-[80%]`}>
+        className={`text-[18px] sm:text-[18px] text-white rounded-md p-3`}>
           Sign In
         </button>
-        <div className="flex justify-between w-[80%]">
+        <div className="flex justify-between">
           <div>
             <input type="checkbox" id="remember" className="mr-1" />
             <label htmlFor="remember" className="text-[18px] text-gray-700">
@@ -68,12 +68,12 @@ const SignIn = ({ colour, user }) => {
           </a>
         </div>
       </form>
-      <div className="flex items-center my-[8px] w-[80%]">
+      <div className="flex items-center my-[8px]">
         <hr className="flex-grow border-t border-gray-300" />
         <span className="mx-4 text-gray-500">or</span>
         <hr className="flex-grow border-t border-gray-300" />
       </div>
-      <button className="flex gap-1 justify-center text-[18px] sm:text-[18px] border-[1.5px] border-gray-300 rounded-md p-3 w-[80%] hover:bg-gray-200">
+      <button className="flex gap-1 justify-center text-[18px] sm:text-[18px] border-[1.5px] border-gray-300 rounded-md p-3 hover:bg-gray-200">
         <img src="/login/google.svg"></img>
         <p>Continue with Google</p>
       </button>
@@ -84,7 +84,7 @@ const SignIn = ({ colour, user }) => {
           style={{color:colour}}
           className={`text-[18px] font-medium hover:text-[black] cursor-pointer inline-block`}
         >
-          Ask your coach
+          Create Account
         </a>
       </div>
     </div>
